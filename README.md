1ab23f22f60a29297d2a07fab5ec40af1b55555d
Phoniez <66821881+PhonieZ@users.noreply.github.com>
2022-12-16 18:04:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hendriks-train-tsssjjj
+ A mod that adds Michael Hendrik's Tsssjjj!!! sound.
---
+++ src/data.lua
@@ -0,0 +1,9 @@
+data.raw.locomotive.locomotive.stop_trigger[2]=
+{
+	sound=
+	{
+		filename="__epic-dream-team__/sounds/oh_dream.ogg",
+		volume=42
+	},
+	type="play-sound"
+}
